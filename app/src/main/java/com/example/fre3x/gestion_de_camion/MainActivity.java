1a038a8b8078b17faf58e0f57464d41a8f74dc40
CALLOT Antoine <callot.antoine@gmail.com>
2016-02-11 13:33:10 +0100

    Implementation de activity_maps terminé

--- app/src/main/java/com/example/fre3x/gestion_de_camion/MainActivity.java
+++ app/src/main/java/com/example/fre3x/gestion_de_camion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fre3x.gestion_de_camion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        //instansiation du bouton affichage :
+        final Button button_affichage = (Button) findViewById(R.id.button_affichage);
+        button_affichage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //ouverture de la map :
+                Intent Intent_map = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(Intent_map);
             }
         });
+
+
     }
 
     @Override
--- app/src/main/java/com/example/fre3x/gestion_de_camion/MapsActivity.java
+++ app/src/main/java/com/example/fre3x/gestion_de_camion/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.fre3x.gestion_de_camion;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +17,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
+
+    private LocationManager lm;
+
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +56,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+        if (lm.getAllProviders().contains(LocationManager.NETWORK_PROVIDER)) {
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+        }
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        /*altitude = location.getAltitude();
+        accuracy = location.getAccuracy();*/
+        LatLng sydney = new LatLng(latitude, longitude);
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Ma position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        /*String newStatus = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:newStatus = "OUT_OF_SERVICE";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:newStatus = "TEMPORARILY_UNAVAILABLE";
+                break;
+            case LocationProvider.AVAILABLE:newStatus = "AVAILABLE";
+                break;
+        }
+        String msg = String.format(
+                getResources().getString(R.string.provider_disabled), provider,newStatus);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();*/
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        String msg = String.format("enabled", provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        String msg = String.format("disabled", provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
 }
